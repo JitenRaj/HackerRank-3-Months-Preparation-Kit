66e9584c3edb8891a105771c9608a70fc4c199db
JitenRaj <ji10raj@gmail.com>
2023-01-04 23:02:49 +0530

    Day 7 : Sparse Arrays Problem Solution

---
+++ Code/src/Week01/Day07/Solution.java
@@ -0,0 +1,88 @@
+package Week01.Day07;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        // Write your code here
+        int numOfQueries = queries.size();
+        int numOfStrings = strings.size();
+
+        HashMap<String,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < numOfStrings; i++) {
+            map.put(strings.get(i),0);
+        }
+
+        for(int i = 0; i < numOfQueries; i++) {
+            map.put(queries.get(i),0);
+        }
+
+        for(int i = 0; i < numOfStrings; i++) {
+            map.put(strings.get(i),map.get(strings.get(i))+1);
+        }
+
+        for(int i = 0; i < numOfQueries; i++) {
+            list.add(map.get(queries.get(i)));
+        }
+
+        return list;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+        List<Integer> res = Result.matchingStrings(strings, queries);
+
+        bufferedWriter.write(
+                res.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
